110dbe69789baf8c58fa6f0259f57511ba6ae246
Kyuho Han <khhan1990@hanmail.net>
2023-06-07 20:45:21 +0900

    프로필 화면 작성중

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { useSelector } from "react-redux";
 
 function Profile() {
-  return <div>Profile</div>;
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <ProfileBlock>
+      <div className="profile">
+        <div className="profile-left">
+          <img
+            className="profile-picture"
+            src={`http://localhost:5000/images/${user.profilePicture}`}
+          />
+          <div className="profile-name">{user.name}</div>
+        </div>
+        <div className="profile-right"></div>
+      </div>
+    </ProfileBlock>
+  );
 }
+
+const ProfileBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  /* border: 1px solid black; */
+  height: 60rem;
+
+  .profile {
+    /* border: 1px solid blue; */
+    width: 100rem;
+    height: 50rem;
+    margin-top: 4rem;
+    display: flex;
+  }
+
+  .profile-left {
+    flex: 3;
+    border: 1px solid red;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .profile-picture {
+    width: 30rem;
+    height: 30rem;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  .profile-name {
+    margin-top: 3rem;
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  .profile-right {
+    flex: 6;
+    height: 100%;
+    border: 1px solid orange;
+  }
+
+  @media (max-width: 800px) {
+  }
+`;
 
 export default Profile;
